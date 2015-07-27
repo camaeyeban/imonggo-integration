736d125edfe0d91672ff7b7ddf69cc342414c37d
Camae <camaeyeban@gmail.com>
2015-07-28 05:53:13 +0800

    Database update

--- imonggo_integration_db.sql
+++ imonggo_integration_db.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 15, 2015 at 12:15 PM
+-- Generation Time: Jul 27, 2015 at 11:51 PM
 -- Server version: 5.6.24
 -- PHP Version: 5.6.8
 
@@ -34,10 +34,21 @@ CREATE TABLE IF NOT EXISTS `adding_customers_option` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `invoices`
+-- Table structure for table `customers`
 --
 
-CREATE TABLE IF NOT EXISTS `invoices` (
+CREATE TABLE IF NOT EXISTS `customers` (
+  `id_3dcart` varchar(30) NOT NULL,
+  `id_imonggo` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invoice_posting`
+--
+
+CREATE TABLE IF NOT EXISTS `invoice_posting` (
   `post_id` smallint(5) unsigned zerofill NOT NULL,
   `post_date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -56,12 +67,34 @@ CREATE TABLE IF NOT EXISTS `last_invoice_posting` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `last_product_posting`
+--
+
+CREATE TABLE IF NOT EXISTS `last_product_posting` (
+  `id` smallint(5) unsigned zerofill NOT NULL,
+  `date` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `products`
 --
 
 CREATE TABLE IF NOT EXISTS `products` (
   `id_imonggo` varchar(30) NOT NULL,
   `id_3dcart` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_posting`
+--
+
+CREATE TABLE IF NOT EXISTS `product_posting` (
+  `post_id` smallint(5) unsigned zerofill NOT NULL,
+  `post_date` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -75,15 +108,27 @@ ALTER TABLE `adding_customers_option`
   ADD PRIMARY KEY (`choice_id`), ADD UNIQUE KEY `choice` (`choice`);
 
 --
--- Indexes for table `invoices`
+-- Indexes for table `customers`
 --
-ALTER TABLE `invoices`
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`id_3dcart`), ADD UNIQUE KEY `id_imonggo` (`id_imonggo`);
+
+--
+-- Indexes for table `invoice_posting`
+--
+ALTER TABLE `invoice_posting`
   ADD PRIMARY KEY (`post_id`), ADD UNIQUE KEY `post_date` (`post_date`);
 
 --
 -- Indexes for table `last_invoice_posting`
 --
 ALTER TABLE `last_invoice_posting`
+  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `date` (`date`), ADD UNIQUE KEY `date_2` (`date`);
+
+--
+-- Indexes for table `last_product_posting`
+--
+ALTER TABLE `last_product_posting`
   ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `date` (`date`);
 
 --
@@ -91,6 +136,12 @@ ALTER TABLE `last_invoice_posting`
 --
 ALTER TABLE `products`
   ADD PRIMARY KEY (`id_imonggo`), ADD UNIQUE KEY `id_3dcart` (`id_3dcart`);
+
+--
+-- Indexes for table `product_posting`
+--
+ALTER TABLE `product_posting`
+  ADD PRIMARY KEY (`post_id`), ADD UNIQUE KEY `post_date` (`post_date`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -102,15 +153,25 @@ ALTER TABLE `products`
 ALTER TABLE `adding_customers_option`
   MODIFY `choice_id` smallint(5) unsigned zerofill NOT NULL AUTO_INCREMENT;
 --
--- AUTO_INCREMENT for table `invoices`
+-- AUTO_INCREMENT for table `invoice_posting`
 --
-ALTER TABLE `invoices`
+ALTER TABLE `invoice_posting`
   MODIFY `post_id` smallint(5) unsigned zerofill NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `last_invoice_posting`
 --
 ALTER TABLE `last_invoice_posting`
   MODIFY `id` smallint(5) unsigned zerofill NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `last_product_posting`
+--
+ALTER TABLE `last_product_posting`
+  MODIFY `id` smallint(5) unsigned zerofill NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `product_posting`
+--
+ALTER TABLE `product_posting`
+  MODIFY `post_id` smallint(5) unsigned zerofill NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
